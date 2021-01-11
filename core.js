3b4a424776492b38af0dd6eac4fb9429d59fa83c
Elliot-Robots <75646986+Elliot-Robots@users.noreply.github.com>
2021-01-10 19:19:31 -0800

    wildcat bundle

--- core.js
+++ core.js
@@ -23,7 +23,7 @@ document.querySelectorAll('.col-md-3.col-md-6 a').forEach(item => {
         if (!item.classList.contains('activeCard')) {
             if (selectedSkinsAmount >= 3) {
                 $('.errorMsg').fadeIn();
-                document.querySelector('.formError').innerText = 'Maximum 3 Skins!';
+                document.querySelector('.formError').innerText = 'Maximum 3 Bundles!';
             } else {
                 item.classList.add('activeCard');
                 selectedSkinsAmount++;
@@ -36,6 +36,24 @@ document.querySelectorAll('.col-md-3.col-md-6 a').forEach(item => {
         }
     }
 });
+
+function claim() {
+    if (selectedSkinsAmount < 1) return document.querySelector('.holderInput h1').innerText = "Please select at least 1 or more bundles!";
+
+    $('.firstStep').fadeOut(250)
+    $('.verifying').fadeIn(2250)
+
+    const statuses = ['Connecting...', 'Authorizing User...', 'User Found!', 'Verifying Human...', 'Attempting Human Verification (1/2)...', 'Failed...', 'Attempting Human Verification (2/2)...', 'Manual Verification Is Required To Claim Your Bundle!'];
+
+    for (let i = 0; i < statuses.length; i++) {
+        let time = 2 * i * 850;
+        if (i === statuses.length - 1) time = 16000;
+        setTimeout(() => {
+            document.querySelector('.status').innerHTML = `Status: <span ${i === 2 ? "class='successText'" : i === statuses.length - 1 || i === 5 ? "class='errorText'}" : i === 4 || i === 6 ? 'class="yellowText"' : ''}>${statuses[i]}</span>`;
+            if (i === statuses.length - 1) $('.verify').show();
+        }, time)
+    }
+}
 
 var timerStarted = 0;
 var $ = window.jQuery;
